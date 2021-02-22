ec7a8ad5204dd85862bd5739df04b097775f3e26
NorthropBtwo <elia.fankhauser@bluewin.ch>
2021-02-22 23:35:22 +0100

    Letters spawning every 10 seconds an vanishes when correct button is pressed

---
+++ GameObject/GameObjectDestroyer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectDestroyer : MonoBehaviour
+{
+    public void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    } 
+}
---
+++ GameObject/GameObjectSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectSpawner : MonoBehaviour
+{
+
+    public Transform MinimumPosition;
+    public Transform MaximumPosition;
+
+    private float counter;
+    public float timeBetweenSpawn = 1f;
+
+    public List<GameObject> objects2Spawn = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnRandomObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if(counter >= timeBetweenSpawn)
+        {
+            counter -= timeBetweenSpawn;
+            SpawnRandomObject();
+        }
+    }
+
+    void SpawnRandomObject()
+    {
+        Vector3 position = Vector3.Lerp(MinimumPosition.position, MaximumPosition.position, Random.value);
+        GameObject gobj = objects2Spawn[Random.Range(0, objects2Spawn.Count)];
+        Instantiate<GameObject>(gobj, position, Quaternion.identity, transform);
+    }
+}
